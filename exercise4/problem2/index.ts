f1586d44dbdece373db60f12f4b909ab13f6bd44
Danial-Doskuzhinov <doskuzhinovdanial@gmail.com>
2023-10-24 17:39:42 +0600

    add hw4

--- exercise4/problem2/index.ts
+++ exercise4/problem2/index.ts
@@ -1,3 +1,18 @@
-class Name {}
+class Name {
+    firstName:string;
+    lastName:string;
+  constructor(firstName:string, lastName:string) {
+    this.firstName = firstName[0].toUpperCase() + firstName.slice(1).toLowerCase();
+    this.lastName = lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
+  }
+
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  get initials() {
+    return this.firstName[0] + '.' + this.lastName[0];
+  }
+}
 
 export default Name;
--- exercise4/problem3/index.ts
+++ exercise4/problem3/index.ts
@@ -1,8 +1,29 @@
 // Update it as much as you want, just don't change the name
-export class Circle {}
+export class Circle {//3.14 * r *r 
+    circle:number;
+    
+    constructor(circle:number){
+         this.circle = circle 
+    }
+area(){
+    return Math.PI * this.circle * this.circle
+}
+}
 
 // Update it as much as you want, just don't change the name
-export class Rectangle {}
+export class Rectangle {
+    rect:number;
+    rectt:number
+    constructor(rect:number, rectt:number){
+       this.rect = rect
+       this.rectt = rectt
+    }
+area(){
+    return this.rect * this.rectt;
+}
+}
 
 // Update it as much as you want, just don't change the name
-export function sumOfAllAreas() {}
+export function sumOfAllAreas(a:Circle | Rectangle,b: Circle | Rectangle){
+    return a.area() + b.area()
+    } 
