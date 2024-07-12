17cf06706f778d57b5dff924b89be3ba0fa4636f
Danial <143175394+Danial-Doskuzhinov@users.noreply.github.com>
2024-07-12 22:42:28 +0500

    Delete exercise5 directory

--- exercise5/problem1/index.ts
+++
@@ -1,9 +0,0 @@
-function callbackExec(callb:string) {
-    setTimeout(callb,2000)
-}
-function displayHello(){
-    console.log('Hello');
-}
-
-callbackExec(displayHello);
-export default callbackExec;
--- exercise5/problem5/index.ts
+++
@@ -1,8 +0,0 @@
-function wait(ms: number) {
-  // Your code
-  return new Promise((resolve)=>{
-    setTimeout(resolve, ms)
-  })
-}
-
-export default wait;
