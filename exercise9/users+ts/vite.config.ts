25c031f85871fdc4575f42c153fc10a511a88e08
Danial <143175394+Danial-Doskuzhinov@users.noreply.github.com>
2024-07-12 23:05:53 +0500

    Update vite.config.ts

--- exercise9/users+ts/vite.config.ts
+++ exercise9/users+ts/vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-    base:"/github-search/exercise9",
+    base:"/github-search/exercise9/users+ts",
 })
